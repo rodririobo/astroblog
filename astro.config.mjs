604cd9e51f31c1b5e0b55e00f3cc64365d80de15
Roberto <rodri.riobo@gmail.com>
2024-04-16 17:18:38 +0200

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://rodririobo.github.io",
-	base: '/astroblog',
+	
+    site: "https://rodririobo.github.io",
+    base: "/astroblog",
 	integrations: [mdx(), sitemap()],
+	output: "hybrid",
+	adapter: node({
+		mode: "standalone"
+	})
 });
