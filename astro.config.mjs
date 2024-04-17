f4fdbbf93b670d79341fff291579fdaab3faeaad
Roberto <64167140+rodririobo@users.noreply.github.com>
2024-04-17 10:34:01 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import node from "@astrojs/node";
+//import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,10 +12,10 @@ export default defineConfig({
 
 	integrations: [mdx(), sitemap()],
 
-	output: "hybrid",
+	//output: "hybrid",
 
-	adapter: node({
-		mode: "standalone"
+	//adapter: node({
+		//mode: "standalone"
 	})
 
 });
