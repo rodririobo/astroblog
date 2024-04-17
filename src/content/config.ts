5cb1293a2ff6c199e106bd470d8e7866267a2a8d
Roberto <rodri.riobo@gmail.com>
2024-04-17 15:51:19 +0200

    update

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,28 @@
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		author: z.string().optional(),
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+  type: 'content',
+  // Type-check frontmatter using a schema function
+  schema: () => {
+    const baseUrl = import.meta.env.BASE_URL;
+
+    return z.object({
+      title: z.string(),
+      description: z.string(),
+      author: z.string().optional(),
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      // Assuming heroImage is a relative URL, prepend it with the baseUrl
+      heroImage: z.string().optional().refine((value) => {
+        // If heroImage is provided and it starts with the expected prefix
+		console.log("Entrando en la función refine");
+        if (value && value.startsWith(baseUrl)) {
+          return true; // Return true if it meets the validation criteria
+        }
+        return false; // Otherwise, return false
+      }, { message: 'Hero image URL must start with the base URL.' }),
+    });
+  },
 });
 
 export const collections = { blog };
